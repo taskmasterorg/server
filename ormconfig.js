820088bda23f6a832e1e5f9ac9de4e10dd7bd9ae
viveknathani <viveknathani2402@gmail.com>
2021-08-27 19:36:55 +0530

    chore: setup the codebase
    This marks the beginning of this codebase. Let the commits tell you a
    story.

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "type": "postgres",
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "synchronize": true,
+    "logging": true,
+    "entities": ["build/db/entity/*.js"]
+};
