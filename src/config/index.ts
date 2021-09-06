e00a6d5b42cd76ef4cf432eabc69227d3ba208b3
viveknathani <viveknathani2402@gmail.com>
2021-09-06 23:06:00 +0530

    feat: add support for JWT

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export default {
     PORT: process.env.PORT || 4000,
-    NODE_ENV: process.env.NODE_ENV || "development"
+    NODE_ENV: process.env.NODE_ENV || "development",
+    JWT_SECRET: process.env.JWT_SECRET || 'somesecret',
 }
