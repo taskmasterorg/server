966c32f2115e8cab25e89b06ca1c48c4850e6b01
viveknathani <viveknathani2402@gmail.com>
2021-09-22 16:43:21 +0530

    refactor: unused import

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import { createConnection, createConnections } from 'typeorm';
+import { createConnection } from 'typeorm';
 import Bug from './entity/Bug';
 import Chat from './entity/Chat';
 import Organization from './entity/Organization';
