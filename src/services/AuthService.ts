7115cb12a63125c246bc2d4edfc5f3427e3fab52
viveknathani <viveknathani2402@gmail.com>
2021-10-25 18:07:17 +0530

    fix: incorrect scope for regex

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -154,7 +154,7 @@ class AuthService {
         const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         
         //eslint-disable-next-line
-        const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        const regexPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
 
         return regexEmail.test(email) && regexPassword.test(password);
     }
